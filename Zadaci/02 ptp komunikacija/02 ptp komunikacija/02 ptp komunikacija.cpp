60788417ec80dc648c96d3503423109ca6cb3a0f
Aleksa <jevticaleksa553@gmail.com>
2026-03-21 18:42:05 +0100

    Added example "03 ptp komunikacija", works only with powers of 2

---
+++ Zadaci/02 ptp komunikacija/02 ptp komunikacija/02 ptp komunikacija.cpp
@@ -0,0 +1,35 @@
+﻿//Zadatak 2: Napisati program koji nalazi sumu n celih brojeva koriscenjem ptp 
+//komunikacije tako da svaki proces ucestvuje u sumiranju.
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	int value=0, sum=0, rank, size;
+	MPI_Status status;
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == 0 && rank + 1 < size) {
+		value = 1;
+		MPI_Send(&value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+	}
+	else {
+		MPI_Recv(&value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &status);
+		sum = value + rank + 1;
+		if (rank < size - 1) {
+			MPI_Send(&sum, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+		}
+		if (rank == size - 1) {
+			printf("Suma: %d", sum);
+		}
+	}
+
+	MPI_Finalize();
+	return 0;
+}
+
---
+++ Zadaci/03 ptp komunikacija/03 ptp komunikacija/03 ptp komunikacija.cpp
@@ -0,0 +1,50 @@
+﻿#include <mpi.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	int sum = 0, MyRank=0, Source_tag, Destination_tag;
+	int NoofLevels, Level, NextLevel, ilevel;
+	int value = 0, Source = 0, Destination = 0;
+	int Root = 0;
+	MPI_Status status;
+
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &MyRank);
+
+	sum = MyRank + 1;
+	Source_tag = 0;
+	Destination_tag = 0; 
+
+	NoofLevels = (int)(log((double)size) / log(2.0));
+	for (ilevel = 0; ilevel < NoofLevels; ilevel++) {
+		Level = (int)(pow((double)2, (double)ilevel));
+		if ((MyRank % Level) == 0) {
+			NextLevel = (int)(pow((double)2, (double)(ilevel+1)));
+			if ((MyRank % NextLevel) == 0) {
+				Source = MyRank + Level;
+				if (Source <= size - 1) {
+					MPI_Recv(&value, 1, MPI_INT, Source, Source_tag,
+						MPI_COMM_WORLD, &status);
+					sum = sum + value;
+					printf("%d partial sum: %d\n", MyRank, sum);
+					fflush(stdout);
+				}
+			}
+			else {
+				Destination = MyRank - Level;
+				MPI_Send(&sum, 1, MPI_INT, Destination, 
+					Destination_tag, MPI_COMM_WORLD);
+			}
+		}
+	}
+	if (MyRank == Root)
+		printf("%d Final SUM %d\n", MyRank, sum);
+
+	MPI_Finalize();
+	return 0;
+}
